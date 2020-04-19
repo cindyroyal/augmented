50c6b153bfaa43fb0e0f1da0614522965c80fbe3
Cindy Royal <5922983+cindyroyal@users.noreply.github.com>
2020-04-19 17:49:43 -0500

    Add files via upload

---
+++ newReality2/newReality2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newReality2
+//
+//  Created by Royal, Cindy L on 4/19/20.
+//  Copyright © 2020 Royal, Cindy L. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
